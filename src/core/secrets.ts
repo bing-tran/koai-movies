220bd5458160467a00cb830363d0c894c7e468b2
Bing Tran <bing.tranle@gmail.com>
2020-12-04 00:27:33 +0700

    Config env variable by dotenv

--- src/core/secrets.ts
+++ src/core/secrets.ts
@@ -1,7 +1,10 @@
-const { NODE_ENV, HOST, PORT } = process.env;
+// const { NODE_ENV, HOST, PORT } = process.env;
+// console.log(process.env);
+// console.log(HOST);
+// console.log(PORT);
 
-export default {
-    IS_PROD: NODE_ENV === 'production',
-    HOST: typeof HOST === 'undefined' ? 'localhost' : HOST,
-    PORT: typeof PORT === 'undefined' ? 5009 : parseInt(PORT, 4),
-};
+// export default {
+//     IS_PROD: NODE_ENV === 'production',
+//     HOST: typeof HOST === 'undefined' ? 'localhost' : HOST,
+//     PORT: typeof PORT === 'undefined' ? 5009 : parseInt(PORT, 4),
+// };
